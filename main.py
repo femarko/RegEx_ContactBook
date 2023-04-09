025d4f03c4217a9e83b3ab4ecc96654ac441163d
Марк <fenster.mark@yandex.ru>
2023-04-10 00:38:04 +0300

    in the pr...

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ with open("phonebook_raw.csv", encoding='utf8') as f:
 pprint(contacts_list)
 
 # 1. Выполните пункты 1-3 задания.
+
+# ищем ФИО
 name_pattern = r'\w+'
+not_name_pattern = r',+'
 names_list = []
+not_names_list = []
 for contact in contacts_list[1:]:
     words = re.findall(name_pattern, str(contact))
     names_list.append(words[0:3])
+    rest = re.split(not_name_pattern, str(contact))
+    not_names_list.append(rest)
 pprint(names_list)
+pprint(not_names_list)
 
-for name in names_list:
-    print(name)
+# обрабатываем остальное
+# for
+# for name in names_list:
+#     print(name)
 '''
 если contact[0] не содержит запятых...; если содержит только одну...; если содержит две - ничего не делать
 если 
