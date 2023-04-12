e4d252d842947975cf5a41b1e8be0c944834e477
Марк <fenster.mark@yandex.ru>
2023-04-12 18:17:44 +0300

    contacts_dict created for resolving task 3

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ with open("phonebook_raw.csv", encoding='utf8') as f:
     contacts_list = list(rows)
 # pprint(contacts_list)
 
-phone_number_pattern = r'(\+7|8)?[\s(-]*(\d{3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s)*[\s(]*([(])*([доб\.]*)\s*(\d*)[)]*'
+phone_pattern = r'(\+7|8)?[\s(-]*(\d{3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s)*[\s(]*([(])*([доб\.]*)\s*(\d*)[)]*'
 subst = r'+7(\2)\3-\4-\5\6\8\9'
 
+contacts_dict = {}
 for contact_index, contact in enumerate(contacts_list[1:]):
     words = re.findall(r'[А-Яёа-яё]+', str(contact))
     for iteration in range(3):
@@ -18,5 +19,6 @@ for contact_index, contact in enumerate(contacts_list[1:]):
             contact[iteration] = ''
         else:
             contact[iteration] = words[iteration]
-    contacts_list[contact_index][-2] = re.sub(phone_number_pattern, subst, str(contact[-2]))
-pprint(contacts_list)
+    contact[-2] = re.sub(phone_pattern, subst, contact[-2])
+    contacts_dict[contact_index + 1] = dict(zip(contacts_list[0], contact))
+pprint(contacts_dict)
