e22392c8a6b47b0aaf0dc7a5f1cd20ff103720b8
Марк <fenster.mark@yandex.ru>
2023-04-12 03:20:46 +0300

    task 2 +

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ with open("phonebook_raw.csv", encoding='utf8') as f:
     contacts_list = list(rows)
 # pprint(contacts_list)
 
-phone_number_pattern = r'(\+7|8)?[\s(-]*(\d{3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})[\s(]*([доб.]*)\s*(\d*)[)]*'
-subst = r'+7(\2)\3-\4-\5 \6\7'
+phone_number_pattern = r'(\+7|8)?[\s(-]*(\d{3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s)*[\s(]*([(])*([доб\.]*)\s*(\d*)[)]*'
+subst = r'+7(\2)\3-\4-\5\6\8\9'
 
 for contact_index, contact in enumerate(contacts_list[1:]):
     words = re.findall(r'[А-Яёа-яё]+', str(contact))
@@ -20,6 +20,3 @@ for contact_index, contact in enumerate(contacts_list[1:]):
             contact[iteration] = words[iteration]
     contacts_list[contact_index][-2] = re.sub(phone_number_pattern, subst, str(contact[-2]))
 pprint(contacts_list)
-
-
-
