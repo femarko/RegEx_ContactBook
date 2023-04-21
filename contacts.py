a6fb15c3d714f7c9103041319dd3f2abe3467c40
Марк <fenster.mark@yandex.ru>
2023-04-22 00:34:08 +0300

    duplication_per_fields() renaimed in del_dict()

--- contacts.py
+++ contacts.py
@@ -93,9 +93,10 @@ class Contact_book:
             duplicates_dict.setdefault(field, field_list)
         return duplicates_dict
 
-    def duplication_per_fields(self, *fields):
+    def del_dict(self, *fields):
         '''
-    Возвращает словарь, где ключи - индексы полей в словаре self.
+    Возвращает словарь, где ключи - поля телефонной книги, значения - списки индексов записей, подлежащих удалению в
+    соответствующем поле.
         '''
         if len(fields) == 0:
             fields_to_check = self.headers
@@ -103,7 +104,7 @@ class Contact_book:
             fields_to_check = fields
 
         for field in fields_to_check[1:]: # итерируемся по кортежу аргументов, кроме первого аргумента
-            for num, nested_list in enumerate(self.duplicates_compare()[fields[0]]): # итерируемся по списку из [fields_to_check]
+            for num, nested_list in enumerate(self.duplicates_compare()[fields[0]]): # итерируемся по списку из fields_to_check
                 if self.duplicates_compare()[field][num] == self.duplicates_compare()[fields[0]][num]:
                     result = True
                 else:
@@ -112,97 +113,37 @@ class Contact_book:
         if result:
             indeces = self.duplicates_compare()[fields[0]] # т.к. вложенные списки совпали для всех полей, берем первое (field[0])
             the_list = []
-            # iteration = 0
             for header in self.entries_dict().keys():
                 nestedval = []
                 the_list.append(nestedval)
-                # print(f'iteration:{iteration} header:{header} nestedval:{nestedval} the_list:{the_list}')
                 for nested_list in indeces:
                     indexval = []
                     nestedval.append(indexval)
-                    # print(f'\t\t - nested_list:{nested_list} indexval:{indexval} nestedval:{nestedval}')
                     for index in nested_list:
                         indexval.append(self.entries_dict()[header][index])
-                        # print(f'\t\t\tindex:{index} indexval:{indexval}')
-                # iteration += 1
-            # pprint(the_list)
 
-        print(f'indeces: {indeces}')
-        print(f'the_list: {the_list}')
-        # f_list = []
-        dicttt = {}
+        indeces_dict = {}
 
         for field_index, field_values in enumerate(the_list):
-            dicttt[self.headers[field_index]] = {}
+            indeces_dict[self.headers[field_index]] = {}
             for nested_list_index, nested_list in enumerate(field_values):
-                dicttt[self.headers[field_index]][nested_list_index] = []
+                indeces_dict[self.headers[field_index]][nested_list_index] = []
                 for entry_value_index, entry_value in enumerate(nested_list):
-                    dicttt[self.headers[field_index]][nested_list_index].append(len(entry_value))
+                    indeces_dict[self.headers[field_index]][nested_list_index].append(len(entry_value))
 
-        pprint(dicttt)
-
-        for key in dicttt.keys():
-            for key_nest, value in dicttt[key].items():
-                for length in dicttt[key][key_nest]:
+        for key in indeces_dict.keys():
+            for key_nest, value in indeces_dict[key].items():
+                for length in indeces_dict[key][key_nest]:
                     if length == max(value):
-                        dicttt[key][key_nest] = value.index(length)
+                        indeces_dict[key][key_nest] = value.index(length)
 
         del_dict = {}
         for header in self.headers:
             del_dict[header] = []
             for num_list_, list_ in enumerate(indeces):
-                # print(list_)
                 for entr in list_:
-                    if list_.index(entr) != dicttt[header][num_list_]:
+                    if list_.index(entr) != indeces_dict[header][num_list_]:
                         del_dict[header].append(entr)
-        # deletion_dict = {}
-        # for ind, list_of_indeces in indeces:
-        #     for header in self.headers:
-        #         for list_ in indeces:
-        #
-        #         deletion_dict[header] =
-        # final_dict = {}
-        # for key in dicttt.keys():
-        #     final_dict[key] = []
-        #     for key_nest, value in dicttt[key].items():
-        #         max_length = max(value)
-        #         print(value)
-        #         for length in dicttt[key][key_nest]:
-        #             if length == max_length and value.index(length) == 0:
-        #                 continue
-        #             else:
-        #                 final_dict[key].append(length)
-
-        # for key in dicttt.keys():
-        #     final_dict[key] = []
-        #     for nested_length in dicttt.values():
-        #         print(nested_length)
-                # max_nested_length = max(nested_length)
-                # for length in nested_length:
-                #     if length == max_nested_length and nested_length.index(length) == 0:
-                #         continue
-                #     else:
-                #         final_dict[key].append(length)
-
-
-
-        # final_dict = {}
-        #
-        # for key, value in dicttt.items():
-        #     final_dict[key] = []
-        #     for key_n, value_n in value.items():
-        #         max_length = max(dicttt[key][key_n])
-        #         final_dict[key] = [value_n]
-        #         pprint(final_dict)
-        #         # print(max_length)
-                # for length in dicttt[key][key_n]:
-                #     # print(f'{key} {key_n} {length}')
-                #     # if len(set(dicttt[key][key_n])) == len(dicttt[key][key_n]):
-                #     if length != max_length:
-                #             final_dict[key][value_n].append(indeces[key_n][(dicttt[key][key_n].index(length))])
-                #     else:
-                #         final_dict[key][value_n.append(indeces[key_n][0])
-                # pprint(final_dict)
 
         return del_dict
 
